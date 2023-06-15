756d32025d4c2483c0d4a2578b905e8be6fc34e8
Manish Panda <mkpgtr@gmail.com>
2023-06-15 21:57:58 +0530

    making sense of namespacecs & classes in c#

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           MathUtils.Multiply(5, 3);
+           MathUtils.Sum(4, 5);
         }
     }
 }
