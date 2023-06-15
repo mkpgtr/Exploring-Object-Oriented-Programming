68810537462257c1b8ad783064e05685099fb9b6
Manish Panda <mkpgtr@gmail.com>
2023-06-15 21:04:56 +0530

    first-commit

---
+++ Animal.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cSharp
+{
+    public class Animal
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello world");
+        }
+    }
+}
